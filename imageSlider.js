fd7f6357ab8f9ff5795aa9f53044712a77167157
atlas5420 <85279123+atlas5420@users.noreply.github.com>
2021-09-25 21:55:29 +0900

    Add files via upload

--- imageSlider.js
+++ imageSlider.js
@@ -1,31 +1,61 @@
 const images = document.querySelector(".image__slider");
-const prevBtn = document.querySelector(".image__left");
-const nextBtn = document.querySelector(".image__right");
+const imgList = document.querySelectorAll(".image__slider>img");
+const leftBtn = document.querySelector(".image__left");
+const rightBtn = document.querySelector(".image__right");
+let position = 0;
+let leftValue = 5;
+let scale = 0.4;
+let imgsrc ="";
 
-let imageIndex = 0;
-let postion = 0;
-const IMAGE_WIDTH = 400;
-
-function moveleft(){
-  if(imageIndex > 0){
-    postion += IMAGE_WIDTH;
-    images.style.transform = `translateX(${postion}px)`;
-    imageIndex = imageIndex - 1;
-  }
-
+function imgout(){
+  imgList[(Math.floor(imgList.length/2))].style.transform = "scale(1)";
+  imgList[(Math.floor(imgList.length/2))].style.transitionDuration = "1s";
+  
 }
-
+function imgover(){
+  imgList[(Math.floor(imgList.length/2))].style.transform = "scale(2)";
+  imgList[(Math.floor(imgList.length/2))].style.transitionDuration = "1s";
+}
 function moveright(){
-  if(imageIndex <3)
-  postion -= IMAGE_WIDTH;
-    images.style.transform = `translateX(${postion}px)`;
-    imageIndex = imageIndex + 1;
-
-}
-   
-  function init(){
-    prevBtn.addEventListener("click", moveleft);
-    nextBtn.addEventListener("click", moveright);
+  imgsrc=imgList[imgList.length-1].src;
+  for(let i=imgList.length;i>1; i--){
+    imgList[i-1].src=imgList[i-2].src;
   }
-   
-  init();
+  imgList[0].src = imgsrc;
+}
+function moveleft(){
+  imgsrc=imgList[0].src;
+  for(let i=0; i<imgList.length-1; i++){
+    imgList[i].src=imgList[i+1].src;
+  }  
+  imgList[imgList.length-1].src=imgsrc;
+}
+function initialValue(){
+  for(let i=0; i<imgList.length; i++){
+    
+    if(position<(imgList.length/2) && i<(imgList.length/2)){
+      position++;
+      scale += 0.2;
+      imgList[i].style.zIndex = position;
+      imgList[i].style.left = leftValue+"%";
+      imgList[i].style.transform = "scale("+scale+")";
+      leftValue=leftValue+12;
+      
+    }else{
+      position--;
+      scale -= 0.2;
+      imgList[i].style.zIndex = position;
+      imgList[i].style.left = leftValue+"%";
+      imgList[i].style.transform = "scale("+scale+")";
+      leftValue=leftValue+12;
+    }
+  } 
+}
+function init(){
+  initialValue();
+  imgList[(Math.floor(imgList.length/2))].addEventListener('mouseover', function(){imgover();});
+  imgList[(Math.floor(imgList.length/2))].addEventListener('mouseout', function(){imgout();});
+  leftBtn.addEventListener('click', function(){moveleft();});
+  rightBtn.addEventListener('click', function(){moveright();});
+}
+init();
